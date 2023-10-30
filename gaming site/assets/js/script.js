95480e6d3dc84a11d532436771e2c45754a5e09c
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-10-30 11:20:28 +0545

    Update script.js

--- gaming site/assets/js/script.js
+++ gaming site/assets/js/script.js
@@ -9,7 +9,7 @@ const navbarToggler = document.querySelector("[data-nav-toggler]");
 navbarToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
-});
+}); 
 
 for (let i = 0; i < navbarLinks.length; i++) {
   navbarLinks[i].addEventListener("click", function () {
