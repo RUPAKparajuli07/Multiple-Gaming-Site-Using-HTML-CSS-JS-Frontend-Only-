8323f50d6cd0c1e4ff730b73d99600dc38834471
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-10-30 11:20:20 +0545

    Update script.js

--- gaming site/assets/js/script.js
+++ gaming site/assets/js/script.js
@@ -5,7 +5,7 @@
 const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navbarToggler = document.querySelector("[data-nav-toggler]");
-
+ 
 navbarToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
